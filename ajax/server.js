22e7f5fef75d95a6cd6ce6a30b7a276ea8186453
claudiorfj <93228350+claudiorfj@users.noreply.github.com>
2022-07-07 17:49:51 -0300

    ajax update

--- ajax/server.js
+++ ajax/server.js
@@ -6,5 +6,29 @@ app.use(express.static('.'))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
-app.get('/teste',(req, res) => res.send('Ok'))
+const multer = require('multer')
+
+const storage = multer.diskStorage({
+    destination: function(req, file, callback){
+        callback(null, '.')
+    },
+    filename: function(req, file, callback){
+        callback(null, `${Date.now()}_${file.originalname}`)
+    }
+})
+
+const upload = multer({ storage }).single('arquivo')
+
+app.post('/upload', (req, res) => {
+    upload(req, res, err => {
+        if(err){
+            return res.end('Ocorreu um erro.')
+        }
+
+        res.end('Concluido com sucesso.')
+    })
+})
+
 app.listen(8081, () => console.log('Executando...'))
+
+
