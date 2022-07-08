bc25aa65c915c530902a0c28f764bd6eb68a0383
claudiorfj <93228350+claudiorfj@users.noreply.github.com>
2022-07-08 15:33:27 -0300

    gulp udemy update

---
+++ gulp/css/gulpfile.js
@@ -0,0 +1,20 @@
+const { parallel } = require('gulp')
+const gulp = require ('gulp')
+const sass = require ('gulp-sass')
+const uglyfycss = require('gulp-uglifycss')
+const concat = require ('gulp-concat')
+
+function transformacaoCSS(){
+    return gulp.src('src/sass/index.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(uglyfycss({ "uglyComments": true }))
+        .pipe(concat('estilo.min.css'))
+        .pipe(gulp.dest('build/css'))
+}
+
+function copiarHTML(){
+    return gulp.src('src/index.html')
+        .pipe(gulp.dest('build'))
+}
+
+exports.default = parallel(transformacaoCSS, copiarHTML)
