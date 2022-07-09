cf4cfd3d4a82cf57b8c80d585cb4e3b073cad5ac
Cláudio Fernandes <claudiorfj@gmail.com>
2022-07-09 14:51:48 -0300

    webpack udemy news

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,6 +1,26 @@
 const webpack = require('webpack')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports ={
     mode: 'development',
-    entry: './src/principal.js'
+    entry: './src/principal.js',
+    output: {
+        filename: 'principal.js',
+        path: __dirname + '/public'
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "estilo.css"
+        })
+    ],
+    module: {
+        rules: [{
+            test: /\.s?[ac]ss$/,
+            use: [
+                MiniCssExtractPlugin.loader,
+                //'style-loader', // adiciona css a dom injetando a tag <style>
+                'css-loader' //interpreta @impor,url()..
+            ]
+        }]
+    }
 }
