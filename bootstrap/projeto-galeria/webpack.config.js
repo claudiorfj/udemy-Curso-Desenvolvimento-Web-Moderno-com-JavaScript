be4bac57254af4ac77b5a12cb626d3c13b432014
claudiorfj <93228350+claudiorfj@users.noreply.github.com>
2022-07-11 16:37:17 -0300

    projeto-galeria update

--- bootstrap/projeto-galeria/webpack.config.js
+++ bootstrap/projeto-galeria/webpack.config.js
@@ -1,15 +1,15 @@
-const modoDev = process.env.NODE_ENV !== 'production'
-const webpack = require('webpack')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
-const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
-const CopyWebpackPlugin = require('copy-webpack-plugin')
+const modoDev = process.env.NODE_ENV !== "production";
+const webpack = require("webpack");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
-    mode: modoDev ? 'development' : 'production',
-    entry: './src/index.js',
+    mode: modoDev ? "development" : "production",
+    entry: "./src/index.js",
     devServer: {
-        contentBase: './build',
+        contentBase: "./build",
         port: 9000,
     },
     optimization: {
@@ -17,37 +17,41 @@ module.exports = {
             new UglifyJsPlugin({
                 cache: true,
                 parallel: true,
-                sourceMap: true
+                sourceMap: true,
             }),
-            new OptimizeCSSAssetsPlugin({})
-        ]
+            new OptimizeCSSAssetsPlugin({}),
+        ],
     },
     output: {
-        filename: 'app.js',
-        path: __dirname + '/build'
+        filename: "app.js",
+        path: __dirname + "/build",
     },
     plugins: [
-        new MiniCssExtractPlugin({ filename: 'estilo.css' }),
+        new MiniCssExtractPlugin({ filename: "estilo.css" }),
         new CopyWebpackPlugin([
-            { context: 'src/', from: '**/*.html' },
-            { context: 'src/', from: 'imgs/**/*' }
-        ])
+            { context: "src/", from: "**/*.html" },
+            { context: "src/", from: "imgs/**/*" },
+        ]),
     ],
     module: {
-        rules: [{
-            test: /\.s?[ac]ss$/,
-            use: [
-                MiniCssExtractPlugin.loader,
-                // 'style-loader', // Adiciona CSS a DOM injetando a tag <style>
-                'css-loader', // interpreta @import, url()...
-                'sass-loader',
-            ]
-        }, {
-            test: /\.(png|svg|jpg|gif)$/,
-            use: ['file-loader']
-        }, {
-            test: /.(ttf|otf|eot|svg|woff(2)?)$/,
-            use: ['file-loader']
-        }]
-    }
-}
+        rules: [
+            {
+                test: /\.s?[ac]ss$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    // 'style-loader', // Adiciona CSS a DOM injetando a tag <style>
+                    "css-loader", // interpreta @import, url()...
+                    "sass-loader",
+                ],
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: ["file-loader"],
+            },
+            {
+                test: /.(ttf|otf|eot|svg|woff(2)?)$/,
+                use: ["file-loader"],
+            },
+        ],
+    },
+};
