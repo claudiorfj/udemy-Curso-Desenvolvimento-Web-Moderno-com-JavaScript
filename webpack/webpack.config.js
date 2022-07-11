898f8c3608b39fb9972c812ae79c206db4b5d891
claudiorfj <93228350+claudiorfj@users.noreply.github.com>
2022-07-11 09:22:12 -0300

    adicionando img com file-loader

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,12 +1,26 @@
+const modoDev = process.env.NODE_ENV !== 'production'
 const webpack = require('webpack')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const TerserPlugin = require('terser-webpack-plugin')
+const OptimizeCSSAssetsPlugin = require ('optimize-css-assets-webpack-plugin')
 
 module.exports ={
-    mode: 'development',
+    mode: modoDev ? 'development' : 'production',
     entry: './src/principal.js',
     output: {
         filename: 'principal.js',
         path: __dirname + '/public'
+    },
+    optimization: {
+        minimizer: [
+            new TerserPlugin({
+                parallel: true,
+                terserOptions: {
+                    ecma: 6,
+                },
+            }),
+            new OptimizeCSSAssetsPlugin({})
+        ]
     },
     plugins: [
         new MiniCssExtractPlugin({
@@ -19,9 +33,12 @@ module.exports ={
             use: [
                 MiniCssExtractPlugin.loader,
                 //'style-loader', // adiciona css a dom injetando a tag <style>
-                'css-loader', //interpreta @impor,url()..
+                'css-loader', //interpreta @import,url()..
                 'sass-loader',
             ]
+        },{
+            test:/\.(png|svg|jpg|gif)$/,
+            use: ['file-loader']
         }]
     }
 }
