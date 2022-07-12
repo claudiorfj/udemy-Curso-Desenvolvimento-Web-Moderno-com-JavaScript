bcbf509ef096d23a7dc8164012f9b40cbade4474
claudiorfj <93228350+claudiorfj@users.noreply.github.com>
2022-07-12 14:09:10 -0300

    new ionic project

--- ionic/ai-que-dilica/capacitor.config.ts
+++ ionic/ai-que-dilica/capacitor.config.ts
@@ -3,7 +3,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'Ai Que Diliça',
-  webDir: 'www',
+  webDir: 'build',
   bundledWebRuntime: false
 };
 
