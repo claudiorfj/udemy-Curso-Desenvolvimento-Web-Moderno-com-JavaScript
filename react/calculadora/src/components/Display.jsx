2353b1bef6756a2c37db19a058ba3850b201c1fb
claudiorfj <93228350+claudiorfj@users.noreply.github.com>
2022-07-14 16:39:00 -0300

    update calculator project

--- react/calculadora/src/components/Display.jsx
+++ react/calculadora/src/components/Display.jsx
@@ -1,4 +1,7 @@
 import React from "react";
 import "./Display.css";
 
-export default (props) => <div className="display">{props.value}</div>;
+export default props =>
+    <div className="display">
+        {props.value}
+    </div>
