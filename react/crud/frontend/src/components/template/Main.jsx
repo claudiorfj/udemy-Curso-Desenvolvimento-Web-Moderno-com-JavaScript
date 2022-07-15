a29706edba6b2543a1afbe2717f146aa794788c8
claudiorfj <93228350+claudiorfj@users.noreply.github.com>
2022-07-15 09:53:19 -0300

    build components

---
+++ react/crud/frontend/src/components/template/Main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import './Main.css'
+import Header from "./Header";
+
+export default props =>
+    <React.Fragment>
+        <Header>
+            main.content"Conteúdo
+        </Header>
+    </React.Fragment>
