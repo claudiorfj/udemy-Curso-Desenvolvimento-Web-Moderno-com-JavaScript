50b968c3f8155fa50daf433e84522f4543321056
claudiorfj <93228350+claudiorfj@users.noreply.github.com>
2022-07-20 10:55:51 -0300

    exemplos nextjs

--- nextjs/fundamentos/src/pages/index.jsx
+++ nextjs/fundamentos/src/pages/index.jsx
@@ -1,9 +1,12 @@
-export default function Inicio(){
-  // return "Início"
-  return (
+import Link from "next/link";
+
+export default function Inicio() {
+    // return "Início"
+    return (
     <div>
-        <h1>Fundamentos de Next.js & React</h1>
-        <h2>Vamos estudar esse framework</h2>
+      <Link href="/estiloso" >
+
+      </Link>
     </div>
-  )
+    )
 }
