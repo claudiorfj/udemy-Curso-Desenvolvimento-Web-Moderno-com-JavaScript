f81b99be706c242e9ca79df72615c7c5fbaaaf53
claudiorfj <93228350+claudiorfj@users.noreply.github.com>
2022-07-20 15:36:32 -0300

    pre render

---
+++ nextjs/fundamentos/src/pages/estatico.jsx
@@ -0,0 +1,17 @@
+import Layout from "../components/Layout";
+
+export function getStaticProps(){
+    return {
+        props: {
+            numero: Math.random()
+        }
+    }
+}
+
+export default function Estatico(){
+    return(
+        <Layout titulo="Conteúdo Estático">
+            <div>{Math.random()}</div>
+        </Layout>
+    )
+}
