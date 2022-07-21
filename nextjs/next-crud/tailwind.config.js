fff996ce22ab89752f8dc7a09b5b161fad2702da
claudiorfj <93228350+claudiorfj@users.noreply.github.com>
2022-07-21 13:47:00 -0300

    tailwind config

--- nextjs/next-crud/tailwind.config.js
+++ nextjs/next-crud/tailwind.config.js
@@ -1,6 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  purge: [
+    './src/pages/**/*.{js,ts,jsx,tsx}',
+    './src/componentes/**/*.{js,ts,jsx,tsx}'
+  ],
+  darkMode: false,
   content: [],
+  variants:{
+    extend:{},
+  },
   theme: {
     extend: {},
   },
