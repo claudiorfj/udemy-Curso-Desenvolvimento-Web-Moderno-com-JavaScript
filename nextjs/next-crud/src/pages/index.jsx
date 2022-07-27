3335bfe2ed9ddeb2843e710cd24603fd9c1bca99
claudiorfj <93228350+claudiorfj@users.noreply.github.com>
2022-07-27 17:04:34 -0300

    updating components

--- nextjs/next-crud/src/pages/index.jsx
+++ nextjs/next-crud/src/pages/index.jsx
@@ -1,10 +1,34 @@
+import Layout from "../components/Layout"
+import Tabela from "../components/Tabela"
+
 export default function Home() {
+
+  const clientes = [
+    new Cliente('Ana', 34, '1'),
+    new Cliente('Bia', 11, '2'),
+    new Cliente('Carlos', 8, '3'),
+    new Cliente('Pedro', 22, '4'),
+  ]
+
+  function clienteSelecionado(cliente: Cliente){
+   console.log(cliente.nome) 
+  }
+  function clienteExcluido(cliente: Cliente){
+   console.log(`Excluir... ${cliente.nome}`) 
+  }
+
   return (
     <div className={`
       flex h-screen justify-center items-center
       bg-gradient-to-r from-purple-500 via-yellow-500 to-blue-600
+      text-white
     `}>
-      <span className="text-4xl" >Texto</span>
+      <Layout titulo="Cadastro Simples">
+        <Tabela clientes={clientes} 
+          clienteSelecionado={clienteSelecionado}>
+          clienteExcluido={clienteExcluido}
+        </Tabela>
+      </Layout>
     </div>
   )
 }
